6a3c2f8772285cd073e92b095241c6c13a21a266
Fab33500 <lauretfabrice2@protonmail.com>
2022-07-16 19:41:16 +0200

    connexion de la bd et creation des variable d'environnement

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 // imports
 const express = require("express");
 const morgan = require("morgan");
+require("./database/db");
 
 const app = express();
 
@@ -12,6 +13,7 @@ app.use((req, res, next) => {
   next();
 });
 app.use((req, res) => {
+  res.status(201);
   res.json({ message: "Votre requête a bien été reçue !" });
 });
 
