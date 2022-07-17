842eca2e94bcf17dd702852ad80c2a2071c11215
Fab33500 <lauretfabrice2@protonmail.com>
2022-07-17 20:30:20 +0200

    mise en place du middleware force du password

---
+++ backend/middelwares/password.js
@@ -0,0 +1,38 @@
+const passwordValidator = require("password-validator");
+
+// creation du schema
+const passwordSchema = new passwordValidator();
+
+// le schéma que doit respecter le mot de passe
+passwordSchema
+  .is()
+  .min(5) // Longueur Mini 8
+  .is()
+  .max(10) // Longueur Maxi 100
+  .has()
+  .uppercase() // Doit contenir des lettres majuscules
+  .has()
+  .lowercase() // Doit contenir des lettres minuscules
+  .has()
+  .digits(1) // Doit avoir au moins 1 chiffre
+  .has()
+  .not()
+  .spaces() // Ne devrait pas avoir d'espaces
+  .is()
+  .not()
+  .oneOf(["Passw0rd", "Password123"]); // Blacklist les valeurs
+
+module.exports = (req, res, next) => {
+  if (passwordSchema.validate(req.body.password)) {
+    next();
+  } else {
+    return res
+      .status(400)
+      .json({
+        error: `le mot de passe n'est pas assez fort ${passwordSchema.validate(
+          "req.body.password",
+          { list: true }
+        )}`,
+      });
+  }
+};
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,9 +1,10 @@
 // importations
 const express = require("express");
 const userCtrl = require("../controllers/userCtrl");
+const password = require("../middelwares/password");
 
 const router = express.Router();
 
-router.post("/signup", userCtrl.signup);
+router.post("/signup", password, userCtrl.signup);
 
 module.exports = router;
